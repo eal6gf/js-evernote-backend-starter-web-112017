c53cbf755f379b1d8b9b6a82aad96f7a7cfea993
Flatiron School <leslie.hsu@flatironschool.com>
2018-01-17 14:08:19 -0500

    added create new note button & html form for create note

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -2,8 +2,8 @@
 let data = fetch('http://localhost:3000/api/v1/notes') // make a request promise thing.
 .then(res => res.json()) // wait 4 the json to come back!
 .then(res => { // do things with the json
-handleLoadNotes(res)
-handleAddListenersToNotesList()
+handleLoadNotes(res) // add elements to page
+handleAddListenersToNotesList() // add event listener to noteslist area with delegation
 })
 
 function handleLoadNotes(e){
@@ -13,30 +13,37 @@ function handleLoadNotes(e){
     let noteList = document.querySelector('#noteList')
     let newElement = document.createElement('div')
     newElement.innerHTML = `
-    <ul>
-    <li class='noteList'> ${noteInfo.title} </li>
-    <li class='noteList'> ${noteInfo.body} </li>
+    <ul class = 'note'>
+    <li> ${noteInfo.title} </li>
+    <li> ${noteInfo.body} </li>
     </ul>
     `
     noteList.appendChild(newElement)
   })
 }
 
-function handleAddListenersToNotesList(){
-  let notesList = document.querySelectorAll('#noteList')
-  for(let note of notesList) {
-    note.addEventListener('click', handleNoteListClickEvent)
+function handleAddListenersToNotesList() {
+  let notesList = document.querySelector('#noteList')
+  notesList.addEventListener("click", function(e) {
+     if (e.target.parentNode.className === 'note'){
+       let notePanel = document.getElementById('notePanel')
+       // remove child nodes
+       notePanel.innerHTML = ''
+       // add selected node.
+       let x = e.target.parentNode.cloneNode(true)
+       notePanel.appendChild(x)
+    }
   }
+)
 }
 
-function handleNoteListClickEvent(e){
-  //delegation event to handle clicks
-  debugger
-  if (e.target.className === 'noteList'){
-    let notePanel = document.getElementById('notePanel')
-    //remove child nodes
-    notePanel.innerHTML = ''
-    //add selected node.
-    notePanel.appendChild(e.target)
-  }
+function handleNewNoteButtonClick() {
+
+  let form = document.getElementById("formArea")
+  form.style.display = "block"
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let button = document.getElementById("createButton")
+  button.addEventListener("click", handleNewNoteButtonClick)
+})
