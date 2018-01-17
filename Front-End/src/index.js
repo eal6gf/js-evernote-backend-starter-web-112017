1d229dba4e4a5f2b200ce7c58a352426a52c817b
eal6gf <elum1234@gmail.com>
2018-01-17 11:59:15 -0500

    click events

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -1,25 +1,42 @@
 
 let data = fetch('http://localhost:3000/api/v1/notes') // make a request promise thing.
 .then(res => res.json()) // wait 4 the json to come back!
-.then(x => { // do things with the json
-handleLoadNotes(x)
+.then(res => { // do things with the json
+handleLoadNotes(res)
+handleAddListenersToNotesList()
 })
 
-function handleLoadNotes(x){
+function handleLoadNotes(e){
   //do things
-  x.forEach(function(noteInfo){ // needs argument
+  e.forEach(function(noteInfo){ // needs argument
     //forEach vs for (x of collection)
-    let noteList = document.querySelector('.noteList')
+    let noteList = document.querySelector('#noteList')
     let newElement = document.createElement('div')
     newElement.innerHTML = `
     <ul>
-    <li> ${noteInfo.title} </li>
-    <li> ${noteInfo.body} </li>
-
+    <li class='noteList'> ${noteInfo.title} </li>
+    <li class='noteList'> ${noteInfo.body} </li>
     </ul>
     `
     noteList.appendChild(newElement)
   })
 }
 
-function handle
+function handleAddListenersToNotesList(){
+  let notesList = document.querySelectorAll('#noteList')
+  for(let note of notesList) {
+    note.addEventListener('click', handleNoteListClickEvent)
+  }
+}
+
+function handleNoteListClickEvent(e){
+  //delegation event to handle clicks
+  debugger
+  if (e.target.className === 'noteList'){
+    let notePanel = document.getElementById('notePanel')
+    //remove child nodes
+    notePanel.innerHTML = ''
+    //add selected node.
+    notePanel.appendChild(e.target)
+  }
+}
