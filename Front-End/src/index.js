0dc59084428acb5ea375f81515e2c84a9bd6b1b4
eal6gf <elum1234@gmail.com>
2018-01-17 15:31:37 -0500

    it works?

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -1,4 +1,22 @@
 
+class Note {
+  constructor(title, body){
+    this.title = title
+    this.body = body
+  }
+  render() {
+    //renders a new note on the notelist
+    let noteList = document.querySelector('#noteList')
+    let newElement = document.createElement('div')
+    newElement.innerHTML = `
+    <ul class = 'note'>
+    <li> ${this.title} </li>
+    <li> ${this.body} </li>
+    </ul>`
+    noteList.appendChild(newElement)
+  }
+}
+
 let data = fetch('http://localhost:3000/api/v1/notes') // make a request promise thing.
 .then(res => res.json()) // wait 4 the json to come back!
 .then(res => { // do things with the json
@@ -6,20 +24,14 @@ handleLoadNotes(res) // add elements to page
 handleAddListenersToNotesList() // add event listener to noteslist area with delegation
 })
 
-function handleLoadNotes(e){
+function handleLoadSingleNote(title,body){
+  let note = new Note(title,body)
+  note.render()
+}
+
+function handleLoadNotes(dbNotes){
   //do things
-  e.forEach(function(noteInfo){ // needs argument
-    //forEach vs for (x of collection)
-    let noteList = document.querySelector('#noteList')
-    let newElement = document.createElement('div')
-    newElement.innerHTML = `
-    <ul class = 'note'>
-    <li> ${noteInfo.title} </li>
-    <li> ${noteInfo.body} </li>
-    </ul>
-    `
-    noteList.appendChild(newElement)
-  })
+  dbNotes.forEach( (dbNote) => { handleLoadSingleNote(dbNote.title, dbNote.body)})
 }
 
 function handleAddListenersToNotesList() {
@@ -36,9 +48,29 @@ function handleAddListenersToNotesList() {
   }
 )
 }
+function createNewNote() {
+  // get input field
+  let titleInput = document.querySelector("[name=title]").value
+  let bodyInput = document.querySelector("[name=body]").value
+
+  //send to rails api to create a new note on backend --> then front end.
+  fetch('http://localhost:3000/api/v1/notes',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', // what kind of data am i sending?
+        'Accept': 'application/json' // what kind of data do i want back?
+      },
+      body: JSON.stringify({ // pass the data
+        title: titleInput,
+        body: bodyInput,
+      })
+    }).then( () =>
+      handleLoadSingleNote(titleInput,bodyInput)
+
+    )
+  }
 
 function handleNewNoteButtonClick() {
-
   let form = document.getElementById("formArea")
   form.style.display = "block"
 }
